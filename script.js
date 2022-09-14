ee88b262e5bcd467f2500af3f9685dbc65d70f81
ecorina <serban.corrina@gmail.com>
2022-09-15 00:03:35 +0100

    making the date functional

---
+++ script.js
@@ -0,0 +1,35 @@
+let now = new Date();
+console.log(now.getDay());
+
+let today = document.querySelector("#today");
+let date = now.getDate();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+];
+let month = months[now.getMonth()];
+
+today.innerHTML = `${day}, ${date} ${month} ${year}`;
